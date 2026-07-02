0d3bcac1574cd63ab73d1f30aafa3ead5199b265
arwynt <arwyn@taylorkids.us>
2026-07-02 11:05:49 -0600

    wrote program for a number list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userAnswer = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userAnswer != 0)
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+            userAnswer = int.Parse(userResponse);
+
+            if (userAnswer != 0)
+            {
+                numbers.Add(userAnswer);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sume is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
